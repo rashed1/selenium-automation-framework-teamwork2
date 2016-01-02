2002e60b98ec389361888c634ee36175b5df0a09
rashed1 <rashed_023@yahoo.com>
2016-01-02 09:44:41 -0500

    +Base class in Generic Module +AetnaHealth module +Test1: search homepage +Test2: find a doctor page

--- AetnaHealth/src/test/java/homePage/Search.java
+++ AetnaHealth/src/test/java/homePage/Search.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
  * Created by Rashed on 1/2/2016.
  */
 public class Search extends Base {
-    @Test
+    //@Test
     public void test1(){
         clickOnCss("#globalSearch");
         typeByCss("#globalSearch", "insurance");
@@ -17,9 +17,20 @@ public class Search extends Base {
 
 
     @Test
-    public void test2(){
+    public void test2() throws InterruptedException{
         clickByXpath("html/body/div[2]/div[2]/header/div/div/div/nav/ul/li[4]/a");
         System.out.println(driver.getCurrentUrl());
+        clickByXpath(".//*[@id='main']/div/div[1]/div/div/div[2]/div/div");
+
+
+        driver.navigate().to("http://www.aetna.com/dse/search?site_id=dse");
+        typeByCss("#hl-autocomplete-search", "primary care physician");
+        typeByCss("#hl-autocomplete-search-location", "33620");
+        keysInput("#hl-searchbar-button");
+        clickOnCss("#contWOPlan_ft");
+        sleepFor(5);
+        System.out.println(driver.getCurrentUrl());
+
 
     }
 
--- Generic/src/main/java/commonAPI/Base.java
+++ Generic/src/main/java/commonAPI/Base.java
@@ -25,7 +25,7 @@ public class Base {
     @Parameters({"url"})
 
     @BeforeMethod
-    public void setUp(String url){
+    public void setUp(String url) {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.navigate().to(url);
@@ -33,86 +33,100 @@ public class Base {
     }
 
     @AfterMethod
-    public void cleanUp(){
+    public void cleanUp() {
         driver.close();
     }
 
 
     //helper methods
-    public void clickOnCss(String locator){
+    public void clickOnCss(String locator) {
         driver.findElement(By.cssSelector(locator)).click();
     }
 
     //helper methods
-    public void typeByCss(String locator, String value){
+    public void typeByCss(String locator, String value) {
         driver.findElement(By.cssSelector(locator)).sendKeys(value, Keys.CLEAR);
     }
 
 
-
     //helper methods to send ENTER key
-    public void keysInput(String locator){
+    public void keysInput(String locator) {
         driver.findElement(By.cssSelector(locator)).sendKeys(Keys.ENTER);
     }
 
-    public void clickByXpath(String locator){
+    public void clickByXpath(String locator) {
         driver.findElement(By.xpath(locator)).click();
     }
 
-    public void typeByCssThenEnter(String locator, String value){
+    public void typeByCssThenEnter(String locator, String value) {
         driver.findElement(By.cssSelector(locator)).sendKeys(Keys.CLEAR, value, Keys.ENTER);
     }
-    public void typeByXpath(String locator){
+
+    public void typeByXpath(String locator) {
         driver.findElement(By.xpath(locator)).click();
     }
-    public void clickById(String locator){
+
+    public void clickById(String locator) {
         driver.findElement(By.id(locator)).click();
     }
-    public void clickByName(String locator){
+
+    public void clickByName(String locator) {
         driver.findElement(By.id(locator)).click();
     }
-    public void navigateBack(){
+
+    public void navigateBack() {
         driver.navigate().back();
     }
-    public void navigateForward(){
+
+    public void navigateForward() {
         driver.navigate().forward();
     }
-    public String getTextByCss(String locator){
+
+    public String getTextByCss(String locator) {
         String st = driver.findElement(By.cssSelector(locator)).getText();
 
         return st;
     }
-    public String getTextByXpath(String locator){
+
+    public String getTextByXpath(String locator) {
         String st = driver.findElement(By.xpath(locator)).getText();
 
         return st;
     }
-    public String getTextById(String locator){
+
+    public String getTextById(String locator) {
         return driver.findElement(By.id(locator)).getText();
 
     }
-    public String getTextByName(String locator){
+
+    public String getTextByName(String locator) {
         String st = driver.findElement(By.name(locator)).getText();
 
         return st;
     }
-    public WebElement getWebElementByCss(String locator){
+
+    public WebElement getWebElementByCss(String locator) {
         return driver.findElement(By.cssSelector(locator));
 
     }
-    public List<WebElement> getWebElementsByCss(String locator){
+
+    public List<WebElement> getWebElementsByCss(String locator) {
         List<WebElement> elementList = new ArrayList<WebElement>();
         elementList = driver.findElements(By.cssSelector(locator));
 
         return elementList;
     }
-    public WebElement getWebElementByXpath(String locator){
+
+    public WebElement getWebElementByXpath(String locator) {
         WebElement element = driver.findElement(By.xpath(locator));
 
         return element;
     }
 
+    //constant sleep time
+    public void sleepFor(int sec) throws InterruptedException {
+        Thread.sleep(1000 * sec);
 
+    }
 
 }
-
